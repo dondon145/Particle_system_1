b2e071c3086d80ca77095e4ea9b51514226b4ea6
Don <don18donis@gmail.com>
2025-07-18 12:11:19 -0500

    Effect looks somewhat fire color fireworks.

--- main.py
+++ main.py
@@ -16,12 +16,14 @@ BLACK = [0, 0, 0]
 RED = [255, 0, 0, 255]
 BLUE = [0, 0, 255, 255]
 GREEN = [0, 255, 0, 255]
+ORANGE = [255, 100, 0, 255]
+YELLOW = [255, 180, 0, 255]
 
 #obj = particle.Particle(RED, 30, 30, 400, 400, 0.2, 45, 5)
 moving_group = pygame.sprite.Group()
 
 particles = []
-color_list = [RED, BLUE]
+color_list = [RED, ORANGE, YELLOW]
 
 
 running = True
@@ -33,7 +35,7 @@ while running :
 
         if event.type == KEYDOWN:
             for i in range(30):
-                obj = particle.Particle(color_list[random.randrange(0,2)], 30, 30, 400, 400, random.randrange(1, 5), random.randrange(-180, 180), 5, particles)
+                obj = particle.Particle(color_list[random.randrange(0,3)], 30, 30, 400, 400, random.randrange(1, 5), random.randrange(-180, 180), 5, particles)
                 particles.append(obj)
                 moving_group.add(particles[i])
 
